a1adf0aea0eda8c28244fbb02420459ff12329b3
zhuang <258232489@qq.com>
2018-01-11 09:49:06 +0800

    上传示例图

--- app/src/main/java/com/zhuang/zbanner/BannerActivity.java
+++ app/src/main/java/com/zhuang/zbanner/BannerActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.zhuang.zbannerlibrary.PageTransformer.Flip3DTransformer;
 import com.zhuang.zbannerlibrary.ZBanner;
 import com.zhuang.zbannerlibrary.ZBannerAdapter;
 
@@ -37,7 +38,7 @@ public class BannerActivity extends AppCompatActivity {
         }
         ZBanner.ZBannerPageTransformer transformer = list.get(position).transformer;
         if (transformer != null) {
-            zBanner.setPageTransformer(transformer);
+            zBanner.setPageTransformer(new Flip3DTransformer());
         }
     }
 
